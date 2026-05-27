9c533868d2d1a15ec6e292020e33f609178bff66
Aditya Jain <adityajain.jan07@gmail.com>
2026-05-27 22:04:19 +0530

    Add delete notice functionality

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+// const mongoose = require("mongoose");
 
 const app = express();
+
+// mongoose.connect("mongodb://127.0.0.1:27017/campuspulse")
+  // .then(() => console.log("MongoDB connected"))
+  // .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
@@ -29,4 +34,12 @@ app.get("/api/notices", (req, res) => {
 
 app.listen(5050, () => {
   console.log("Server started on port 5050");
+});
+
+app.delete("/api/notices/:index", (req, res) => {
+  const index = req.params.index;
+
+  notices.splice(index, 1);
+
+  res.json({ message: "Notice deleted successfully" });
 });
